41142137771791f5a4bfad98307822ccf6381793
PrafulShinde45 <shindepraful731@gmail.com>
2025-04-01 13:27:04 +0530

    project4

--- innomaticsInternshipTask5/ToDo/vite.config.js
+++ innomaticsInternshipTask5/ToDo/vite.config.js
@@ -1,8 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+  base: './', // Ensures proper asset paths for Netlify deployment
+});
